09e445fc912418fc6ed4e65a5709d268067fdc5c
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-11 10:48:14 +0700

    Update opd.sql

--- sql/hosxpv4_mysql/opd.sql
+++ sql/hosxpv4_mysql/opd.sql
@@ -1,47 +1,47 @@
 SELECT
-	(
-		SELECT
-			hospitalcode
-		FROM
-			opdconfig
-		LIMIT 1
-	) AS HOSPCODE,
-	o.hn AS HN,
-	o.vn AS SEQ,
-	o.main_dep AS CLINIC,
-	DATE_FORMAT(o.vstdate, '%Y%m%d') AS DATE_SERV,
-	DATE_FORMAT(o.vsttime, '%H%i%s') AS TIME_SERV,
-	os.cc AS CHIEFCOMP,
-	os.temperature AS BTEMP,
-	os.bps AS SBP,
-	os.bpd AS DBP,
-	os.pulse AS PR,
-	os.rr AS RR,
-	o.ovstist AS TYPEIN,
-	o.ovstost AS TYPEOUT,
-	o.pttype AS INS_TYPE,
-	o.pttypeno AS INS_NUMBER,
-	o.hospmain AS INS_HOSPMAIN,
-	o.hospsub AS INS_HOSPSUB,
-	o.diag_text AS DIAG_TEXT,
-	DATE_FORMAT(now(), '%Y%m%d%H%i%s') AS D_UPDATE
+  (
+    SELECT
+      hospitalcode
+    FROM
+      opdconfig
+    LIMIT 1
+  ) AS HOSPCODE,
+  o.hn AS HN,
+  o.vn AS SEQ,
+  o.main_dep AS CLINIC,
+  DATE_FORMAT(o.vstdate, '%Y%m%d') AS DATE_SERV,
+  DATE_FORMAT(o.vsttime, '%H%i%s') AS TIME_SERV,
+  os.cc AS CHIEFCOMP,
+  os.temperature AS BTEMP,
+  os.bps AS SBP,
+  os.bpd AS DBP,
+  os.pulse AS PR,
+  os.rr AS RR,
+  o.ovstist AS TYPEIN,
+  o.ovstost AS TYPEOUT,
+  o.pttype AS INS_TYPE,
+  o.pttypeno AS INS_NUMBER,
+  o.hospmain AS INS_HOSPMAIN,
+  o.hospsub AS INS_HOSPSUB,
+  o.diag_text AS DIAG_TEXT,
+  DATE_FORMAT(now(), '%Y%m%d%H%i%s') AS D_UPDATE
 FROM
-	ovst AS o
+  ovst AS o
 INNER JOIN opdscreen AS os on
-	os.vn = o.vn
+  os.vn = o.vn
 WHERE
-	o.vstdate BETWEEN ? AND ?
-	AND EXISTS (
-		SELECT
-					vn
-		FROM
-					ovstdiag od
-		WHERE
-					od.vn = o.vn
-			AND LEFT(
-				od.icd10,
-				1
-			) NOT IN (
-				'1', '2', '3', '4', '5', '6', '7', '8', '9'
-			)
-	);
+  o.vstdate BETWEEN ? AND ?
+  AND EXISTS (
+    SELECT
+      vn
+    FROM
+      ovstdiag od
+    WHERE
+      od.vn = o.vn
+      AND LEFT(
+        od.icd10,
+        1
+      ) NOT IN (
+        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+      )
+  );
