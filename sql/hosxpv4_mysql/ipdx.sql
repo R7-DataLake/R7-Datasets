8c52da386e37b0517056b22623a24f01c5c26911
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-11 11:41:31 +0700

    Create ipdx.sql

---
+++ sql/hosxpv4_mysql/ipdx.sql
@@ -0,0 +1,20 @@
+SELECT
+  (
+    SELECT
+      hospitalcode
+    FROM
+      opdconfig
+    LIMIT 1
+  ) AS HOSPCODE,
+  i.hn AS HN,
+  idx.an AS AN,
+  idx.icd10 AS DIAG,
+  idx.diagtype AS DXTYPE,
+  idx.doctor AS PROVIDER,
+  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as D_UPDATE
+FROM
+  iptdiag AS idx
+INNER JOIN ipt AS i ON
+  i.an = idx.an
+WHERE
+  i.dchdate BETWEEN $1 AND $2;
