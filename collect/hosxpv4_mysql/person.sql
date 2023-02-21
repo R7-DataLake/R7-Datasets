6d5e5fd320b4beb4d0a45bc320619a31031213b6
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-21 16:20:07 +0700

    Remove unused files

--- collect/hosxpv4_mysql/person.sql
+++ collect/hosxpv4_mysql/person.sql
@@ -22,32 +22,4 @@ SELECT
    DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
 FROM
   patient AS p
-WHERE
-  EXISTS (
-    SELECT
-      hn
-    FROM
-      ovst AS o
-    WHERE
-      o.hn = p.hn
-      AND o.vstdate BETWEEN '#{start_date}' AND '#{end_date}'
-      AND 
-      EXISTS (
-        SELECT
-          hn
-        FROM
-          ovstdiag od
-        WHERE
-          od.vn = o.vn
-      )
-  )
-  OR EXISTS (
-    SELECT
-      i.hn
-    FROM
-      ipt AS i
-    WHERE
-      i.hn = p.hn
-      AND i.dchdate BETWEEN '#{start_date}' AND '#{end_date}'
-  )
   GROUP BY p.hn, p.cid;
