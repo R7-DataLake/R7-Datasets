66b462dda3b52bd4d90d8106deb01d7cd2cbbdf2
Satit Rianpit <siteslave@users.noreply.github.com>
2023-02-21 08:57:09 +0700

    Update sql

--- standard/hosxpv3/drug.sql
+++ standard/hosxpv3/drug.sql
@@ -5,14 +5,14 @@ SELECT
     FROM
       opdconfig
     LIMIT 1
-  ) AS HOSPCODE,
-  opi.hn AS HN,
-  opi.an AS AN,
-  opi.vn AS SEQ,
-  opi.icode AS DID,
-  opi.qty AS AMOUNT,
-  opi.unitprice AS DRUGPRICE,
-  opi.cost AS DRUGCOST,
+  ) AS 'HOSPCODE',
+  opi.hn AS 'HN',
+  opi.an AS 'AN',
+  opi.vn AS 'SEQ',
+  opi.icode AS 'DID',
+  opi.qty AS 'AMOUNT',
+  opi.unitprice AS 'DRUGPRICE',
+  opi.cost AS 'DRUGCOST',
   (
     SELECT
       d.units
@@ -21,11 +21,11 @@ SELECT
     WHERE
       d.icode = opi.icode
     LIMIT 1
-  ) AS UNIT,
-  '' AS UNIT_PACK,
-  opi.drugusage AS SIGCODE,
+  ) AS 'UNIT',
+  '' AS 'UNIT_PACK',
+  opi.drugusage AS 'SIGCODE',
   opi.doctor AS PROVIDER,
-  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as D_UPDATE
+  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
 FROM
   opitemrece AS opi
 INNER JOIN ipt AS ip ON
