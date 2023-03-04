52c6960a220031024f07742ad6f06236ae2c2eb0
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-04 08:38:18 +0000

    Fixed marriage status

--- collect/hosxpv4_mysql/person.sql
+++ collect/hosxpv4_mysql/person.sql
@@ -13,11 +13,12 @@ SELECT
   p.lname as 'LNAME',
   p.sex as 'SEX', 
   DATE_FORMAT(p.birthday, "%Y%m%d") as 'BIRTH', 
+  p.occupation as 'OCCUPATION',
+  p.marrystatus as 'MARRIAGE',
   p.nationality as 'NATION',
   p.chwpart as 'CHANGWAT',
   p.amppart as 'AMPHUR',
   p.tmbpart as 'TAMBOL', 
-  p.occupation as 'OCCUPATION',
    p.type_area as 'TYPEAREA',
    DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
 FROM
