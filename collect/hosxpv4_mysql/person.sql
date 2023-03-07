92ee1fe6a4341e04b497f9b8d9801c6e96326e18
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-07 10:23:06 +0700

    Update SQL

--- collect/hosxpv4_mysql/person.sql
+++ collect/hosxpv4_mysql/person.sql
@@ -25,5 +25,5 @@ SELECT
   DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
 FROM
   patient AS p
-  WHERE LENGTH(p.type_area) > 0
+  WHERE LENGTH(p.type_area) > 0 AND LENGTH(p.cid) = 13 AND LENGTH(p.fname) > 0
   GROUP BY p.hn, p.cid;
