724bfb9ce601a2fd3eb69ee11f72361013b5b0b9
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-10 16:28:55 +0700

    fixed moo

--- collect/hosxpv3/person.sql
+++ collect/hosxpv3/person.sql
@@ -14,13 +14,14 @@ SELECT
   p.sex as 'SEX', 
   DATE_FORMAT(p.birthday, "%Y%m%d") as 'BIRTH', 
   p.marrystatus as 'MARRIAGE',
+  p.occupation as 'OCCUPATION',
   p.nationality as 'NATION',
   p.chwpart as 'CHANGWAT',
   p.amppart as 'AMPHUR',
   p.tmbpart as 'TAMBOL', 
-  p.occupation as 'OCCUPATION',
-   p.type_area as 'TYPEAREA',
-   DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
+  p.moopart as 'MOO',
+  p.type_area as 'TYPEAREA',
+  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
 FROM
   patient AS p
 WHERE LENGTH(p.birthday) > 0 AND LENGTH(p.type_area) > 0 AND LENGTH(p.cid) = 13 AND LENGTH(p.fname) > 0
