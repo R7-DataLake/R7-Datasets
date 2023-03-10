18972afb1240994ee7e6b5601e6278f8eaeddfcb
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-10 10:51:53 +0700

    update sql for `ipddrug` and `ipddrug`

--- services/hosxpv3/opddrug.sql
+++ services/hosxpv3/opddrug.sql
@@ -5,47 +5,11 @@ SELECT
     FROM
       opdconfig
     LIMIT 1
-  ) AS 'HOSPCODE',
-  opi.hn AS 'HN',
-  opi.an AS 'AN',
-  opi.vn AS 'SEQ',
-  opi.icode AS 'DID',
-  opi.qty AS 'AMOUNT',
-  opi.unitprice AS 'DRUGPRICE',
-  opi.cost AS 'DRUGCOST',
-  (
-    SELECT
-      d.units
-    FROM
-      drugitems d
-    WHERE
-      d.icode = opi.icode
-    LIMIT 1
-  ) AS 'UNIT',
-  '' AS 'UNIT_PACK',
-  opi.drugusage AS 'SIGCODE',
-  opi.doctor AS PROVIDER,
-  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
-FROM
-  opitemrece AS opi
-INNER JOIN ipt AS ip ON
-  ip.an = opi.an
-  AND ip.dchdate BETWEEN '#{start_date}' AND '#{end_date}'
-GROUP BY opi.hn, opi.vn, opi.icode
-UNION 
-SELECT
-  (
-    SELECT
-      hospitalcode
-    FROM
-      opdconfig
-    LIMIT 1
   ) as 'HOSPCODE',
   opi.hn as 'HN',
-  opi.an as 'AN',
   opi.vn as 'SEQ',
   opi.icode as 'DID',
-  opi.qty as 'AMOUNT',
+  SUM(opi.qty) as 'AMOUNT',
   opi.unitprice as 'DRUGPRICE',
   opi.cost as 'DRUGCOST',
   (
---
+++ services/hosxpv4_mysql/ipddrug.sql
@@ -0,0 +1,32 @@
+SELECT
+  (
+    SELECT
+      hospitalcode
+    FROM
+      opdconfig
+    LIMIT 1
+  ) AS 'HOSPCODE',
+  opi.hn AS 'HN',
+  opi.an AS 'AN',
+  opi.icode AS 'DID',
+  opi.qty AS 'AMOUNT',
+  opi.unitprice AS 'DRUGPRICE',
+  opi.cost AS 'DRUGCOST',
+  (
+    SELECT
+      d.units
+    FROM
+      drugitems d
+    WHERE
+      d.icode = opi.icode
+    LIMIT 1
+  ) AS 'UNIT',
+  '' AS 'UNIT_PACK',
+  opi.drugusage AS 'SIGCODE',
+  opi.doctor AS PROVIDER,
+  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
+FROM
+  opitemrece AS opi
+INNER JOIN ipt AS ip ON ip.an = opi.an
+  AND ip.dchdate BETWEEN '#{start_date}' AND '#{end_date}'
+GROUP BY opi.hn, iop.an, opi.icode;
