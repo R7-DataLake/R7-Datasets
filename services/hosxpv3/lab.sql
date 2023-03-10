f103bc5abc44a9c84ea9df55cf50cf6ee01ff101
Satit Rianpit <siteslave@users.noreply.github.com>
2023-03-10 14:13:42 +0700

    Update `lab` for OPD only

--- services/hosxpv3/lab.sql
+++ services/hosxpv3/lab.sql
@@ -21,28 +21,4 @@ INNER JOIN ovst as o ON
   AND o.vstdate BETWEEN '#{start_date}' AND '#{end_date}'
   AND LENGTH(TRIM(lo.lab_order_result)) > 0
   AND o.an is null
-GROUP BY lh.hn, lh.vn, lo.lab_items_code
-union 
-SELECT 
-  (
-    SELECT
-      hospitalcode
-    FROM
-      opdconfig
-    LIMIT 1
-  ) as 'HOSPCODE',
-  lh.hn as 'HN',
-  lh.vn as 'SEQ',
-  i.an as 'AN',
-  lo.lab_items_code as 'LABTEST',
-  lo.lab_order_result as 'LABRESULT',
-  DATE_FORMAT(now(), '%Y%m%d%H%i%s') as 'D_UPDATE'
-FROM
-  lab_order as lo
-INNER JOIN lab_head as lh ON
-  lh.lab_order_number = lo.lab_order_number
-INNER JOIN ipt as i ON
-  i.vn = lh.vn
-  AND i.dchdate BETWEEN '#{start_date}' AND '#{end_date}'
-  AND LENGTH(TRIM(lo.lab_order_result)) > 0
-GROUP BY lh.hn, i.an, lo.lab_items_code;
+GROUP BY lh.hn, lh.vn, lo.lab_items_code;
